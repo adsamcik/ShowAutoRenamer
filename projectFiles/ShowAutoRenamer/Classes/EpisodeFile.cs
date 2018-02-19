18910552eefced24ebef6e839db273441b990eca
adsamcik <adsamcik@users.noreply.github.com>
2018-02-19 12:30:28 +0100

    Improved episode file

--- projectFiles/ShowAutoRenamer/Classes/EpisodeFile.cs
+++ projectFiles/ShowAutoRenamer/Classes/EpisodeFile.cs
@@ -6,29 +6,35 @@ using System.Threading.Tasks;
 
 namespace ShowAutoRenamer.Classes {
     public class EpisodeFile {
-        public string name {
-            get {
-                return System.IO.Path.GetFileNameWithoutExtension(path);
-            }
-        }
+        public Show Show { get; private set; }
 
-        public string nameWithExtension {
-            get {
-                return System.IO.Path.GetFileName(path);
-            }
-        }
+        public string Name => System.IO.Path.GetFileNameWithoutExtension(path);
 
-        public string folder {
-            get {
-                return System.IO.Path.GetDirectoryName(path);
-            }
-        }
+        public string NameWithExtension => System.IO.Path.GetFileName(path);
+
+        public string Folder => System.IO.Path.GetDirectoryName(path);
+
 
         public string path;
         public bool renaming;
 
-        public EpisodeFile(string path) {
+        public int Season => episode.number;
+        public int EpisodeNumber => episode.season;
+
+        public Episode episode;
+        public bool fromTrakt = false;
+
+        public async Task<Episode> RequestEpisode() {
+            if (!fromTrakt)
+                episode = await Network.GetEpisode(Show, episode.season, episode.number);
+
+            return episode;
+        }
+
+        public EpisodeFile(Show show, string path) {
             this.path = path;
+            this.episode = Functions.GetEpisodeFromName(Name);
+            this.Show = show;
         }
     }
 }
